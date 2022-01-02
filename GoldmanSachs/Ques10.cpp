c95c65eb026b964cd8ce889360ab4d9d5a16f740
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2022-01-02 13:45:22 +0530

    Create Ques10.cpp

---
+++ GoldmanSachs/Ques10.cpp
@@ -0,0 +1,3 @@
+Solution:
+Create a min heap of size 100. Insert the initial 100 elements. Now keep on comparing the elements in array with the smallest element in the heap.
+  If the current element is greater than the smallest element then insert it in the heap and remove the top element. 
