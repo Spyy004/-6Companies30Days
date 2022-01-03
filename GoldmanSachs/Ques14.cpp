a0150b7ec03f028df3b908d29ef69f3a8d3ea090
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2022-01-03 11:36:12 +0530

    Create Ques14.cpp

---
+++ GoldmanSachs/Ques14.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+       int ans=INT_MAX,sum=0,i=0,j=0,n=nums.size();
+       while(j<n)
+       {
+           sum+=nums[j];
+           while(sum>=target && i<=j)
+           {
+               ans=min(ans,j-i+1);
+               sum-=nums[i];
+               i++;
+           }
+           j++;
+       }
+        return ans==INT_MAX?0:ans;
+    }
+};
