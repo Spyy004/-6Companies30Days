94a72561a90e77f788b917b725be4460dc099f21
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2022-01-15 13:34:36 +0530

    Create Ques14.cpp

---
+++ Microsoft/Ques14.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    int minSteps(int D){
+        int steps = 0, sum=0;
+         while(true){
+            sum = sum + steps;
+            steps++;
+            if(sum == D){
+                return steps-1;
+                break;
+            }
+            if(sum > D and (sum-D)%2 == 0){
+                return steps-1;
+                break;
+            }
+        }
+        return 0;
+    }
+};
