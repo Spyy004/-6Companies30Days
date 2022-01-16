bf1ab3fbfde204e3e7c065ba065534e71c895eae
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2022-01-16 12:28:18 +0530

    Create Ques2.cpp

---
+++ Adobe/Ques2.cpp
@@ -0,0 +1,17 @@
+  int ans = 0;
+       if(n == 1) return 1;
+
+        vector<vector<int>>dp(n, vector<int>(10001));
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+
+                int d = nums[j] - nums[i];
+
+                dp[j][d] = max(dp[i][d] + 1, 2);
+
+                ans = max(ans, dp[j][d]);
+            }
+        }
+
+        return ans;
